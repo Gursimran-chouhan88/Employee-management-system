491d9b1c6d7caad2f621468de950fd1d7dc86a20
Gursimran Chouhan88 <gursimranjeet0786@gmail.com>
2024-12-31 15:34:27 +0530

    Add backend and frontend configurations, implement user authentication, and create leave management features

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
